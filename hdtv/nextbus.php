e545a50086ac2a07c6e905e36783d58144c1ec63
PKMN7 <jl@nwk-127-234.nwk.ruw.rutgers.edu>
2017-02-22 15:41:30 -0500

    jdsnjkdsc
    sfdicndsicnkdj

---
+++ hdtv/nextbus.php
@@ -0,0 +1,33 @@
+<center><span style="color:#cc5500; font-size: 32px; font-weight: bold;">NextBus Predictions</span></center>
+<font color=white size=3>
+<?xml version="1.0" encoding="utf-8" ?>
+<body copyright="All data copyright Rutgers University 2017.">
+<predictions agencyTitle="Rutgers University" routeTitle="All Campuses" routeTag="s" stopTitle="Hill Center" stopTag="hillw" dirTitleBecauseNoPredictions="To Livingston Student Center">
+</predictions>
+<predictions agencyTitle="Rutgers University" routeTitle="REX B" routeTag="rexb" stopTitle="Hill Center" stopTag="hillw" dirTitleBecauseNoPredictions="To Allison Road Classrooms">
+</predictions>
+<predictions agencyTitle="Rutgers University" routeTitle="H" routeTag="h" stopTitle="Hill Center" stopTag="hilln" dirTitleBecauseNoPredictions="To Rutgers Student Center">
+</predictions>
+<predictions agencyTitle="Rutgers University" routeTitle="Weekend 1" routeTag="wknd1" stopTitle="Hill Center" stopTag="hillw">
+  <direction title="To Livingston Student Center">
+  <prediction epochTime="1486847633667" seconds="1178" minutes="19" isDeparture="false" affectedByLayover="true" dirTag="wknd1_outbound" vehicle="4193" block="WK1R03" />
+  <prediction epochTime="1486850333718" seconds="3878" minutes="64" isDeparture="false" affectedByLayover="true" dirTag="wknd1_outbound" vehicle="4191" block="WK1R02" />
+  <prediction epochTime="1486851773718" seconds="5318" minutes="88" isDeparture="false" affectedByLayover="true" dirTag="wknd1_outbound" vehicle="4193" block="WK1R03" />
+  </direction>
+</predictions>
+<predictions agencyTitle="Rutgers University" routeTitle="B" routeTag="b" stopTitle="Hill Center" stopTag="hillw" dirTitleBecauseNoPredictions="To Busch Student Center">
+</predictions>
+<predictions agencyTitle="Rutgers University" routeTitle="A" routeTag="a" stopTitle="Hill Center" stopTag="hillw" dirTitleBecauseNoPredictions="To Busch Student Center">
+</predictions>
+<predictions agencyTitle="Rutgers University" routeTitle="Weekend 2" routeTag="wknd2" stopTitle="Hill Center" stopTag="hilln">
+  <direction title="To Rutgers Student Center">
+  <prediction epochTime="1486846780712" seconds="325" minutes="5" isDeparture="false" dirTag="wknd2_inbound" vehicle="4208" block="WK2R01" />
+  <prediction epochTime="1486848043047" seconds="1587" minutes="26" isDeparture="false" affectedByLayover="true" dirTag="wknd2_inbound" vehicle="4195" block="WK2R02" />
+  <prediction epochTime="1486849309915" seconds="2854" minutes="47" isDeparture="false" affectedByLayover="true" dirTag="wknd2_inbound" vehicle="4180" block="WK2R03" />
+  <prediction epochTime="1486851643047" seconds="5187" minutes="86" isDeparture="false" affectedByLayover="true" dirTag="wknd2_inbound" vehicle="4195" block="WK2R02" />
+  </direction>
+</predictions>
+<predictions agencyTitle="Rutgers University" routeTitle="C" routeTag="c" stopTitle="Hill Center" stopTag="hillw" dirTitleBecauseNoPredictions="To Allison Road Classrooms">
+</predictions>
+</body>
+<center><table width=250 style="color:white;font-size:24px;"><tr><td style="text-align:right;width:auto;">Weekend 1</td><td style="text-align:center;width:10px;"> - </td><td style="text-align:left;">19:38</td></tr><tr><td style="text-align:right;width:auto;">Weekend 2</td><td style="text-align:center;width:10px;"> - </td><td style="text-align:left;">5:25</td></tr></table><i><font size=2>Last updated at 03:55:15</font></i></center></font>
